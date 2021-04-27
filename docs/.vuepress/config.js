212eecaa14987b03200c332d41009fa84a536418
xjcheng4 <xjcheng4@iflytek.com>
2021-04-27 17:03:52 +0800

    add: init 🍊

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // base: '/123',
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    lastUpdated: true,
+    lastUpdated: 'Last Updated',
+    themeConfig: {
+      nav: [
+        { text: 'External', link: 'https://www.baidu.com', target:'_self', rel:'', target:'_blank' },
+        { text: 'Guide', link: '/guide/' }
+      ],
+      sidebar: [
+        '/',
+        '/guide/'
+      ]
+    }
+  }
---
+++ docs/guide/README.md
@@ -0,0 +1,20 @@
+---
+title: Blogging Like a Hacker
+lang: en-US
+sidebarDepth: 2
+collapsable: false
+---
+
+# 1111111
+
+## aa
+
++ ssadsdsddsds
+
+## bb
+
+### cc
+
+#### ddd
+
++ bbb
